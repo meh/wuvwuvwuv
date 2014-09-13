74cc9ac5512fd1418af772b424e975574e595566
meh <meh@schizofreni.co>
2014-09-13 16:19:10 +0200

    map/eternal: add missing chat links

--- app/map/eternal.rb
+++ app/map/eternal.rb
@@ -18,6 +18,7 @@ class Map
 			type :tower
 			cardinal :ww
 			location -26110.56, -12943.2
+			link '[&BPcDAAA=]'
 		end
 
 		objective do
@@ -26,6 +27,7 @@ class Map
 			type :camp
 			cardinal :w
 			location -22010.16, -2349.6
+			link '[&BPgDAAA=]'
 		end
 
 		objective do
@@ -34,6 +36,7 @@ class Map
 			type :tower
 			cardinal :w
 			location -12559.92, -5227.2
+			link '[&BPkDAAA=]'
 		end
 
 		objective do
@@ -42,6 +45,7 @@ class Map
 			type :keep
 			cardinal :sw
 			location -19802.4, -17690.4
+			link '[&BPMDAAA=]'
 		end
 
 		objective do
@@ -50,6 +54,7 @@ class Map
 			type :tower
 			cardinal :sw
 			location -5013.6, -17596.8
+			link '[&BPQDAAA=]'
 		end
 
 		objective do
@@ -58,6 +63,7 @@ class Map
 			type :camp
 			cardinal :sw
 			location -7497.6, -25468.8
+			link '[&BPUDAAA=]'
 		end
 
 		objective do
@@ -66,6 +72,7 @@ class Map
 			type :tower
 			cardinal :ssw
 			location 15682.08, -25161.6
+			link '[&BPYDAAA=]'
 		end
 
 		objective do
@@ -74,6 +81,7 @@ class Map
 			type :tower
 			cardinal :nnw
 			location -5599.2, 22768.8
+			link '[&BO8DAAA=]'
 		end
 
 		objective do
@@ -81,6 +89,7 @@ class Map
 			name 'Red Mill', 'Speldan Clearcut', 'Speldan'
 			type :camp
 			cardinal :nw
+			link '[&BO0DAAA=]'
 		end
 
 		objective do
@@ -89,6 +98,7 @@ class Map
 			type :tower
 			cardinal :nw
 			location -16848.72, 19048.8
+			link '[&BOwDAAA=]'
 		end
 
 		objective do
@@ -97,6 +107,7 @@ class Map
 			type :keep
 			cardinal :n
 			location 4548, 17078.4
+			link '[&BOsDAAA=]'
 		end
 
 		objective do
@@ -105,6 +116,7 @@ class Map
 			type :tower
 			cardinal :ne
 			location 12072, 10622.4
+			link '[&BPEDAAA=]'
 		end
 
 		objective do
@@ -113,6 +125,7 @@ class Map
 			type :camp
 			cardinal :ne
 			location 18393.6, 13939.2
+			link '[&BPADAAA=]'
 		end
 
 		objective do
@@ -121,6 +134,7 @@ class Map
 			type :tower
 			cardinal :nne
 			location 13173.6, 2058
+			link '[&BO4DAAA=]'
 		end
 
 		objective do
@@ -129,6 +143,7 @@ class Map
 			type :tower
 			cardinal :ee
 			location 29232, -11200.8
+			link '[&BOYDAAA=]'
 		end
 
 		objective do
@@ -137,6 +152,7 @@ class Map
 			type :camp
 			cardinal :e
 			location 25802.4, -2716.8
+			link '[&BOQDAAA=]'
 		end
 
 		objective do
@@ -145,6 +161,7 @@ class Map
 			type :tower
 			cardinal :e
 			location 17412, -5109.6
+			link '[&BOMDAAA=]'
 		end
 
 		objective do
@@ -153,6 +170,7 @@ class Map
 			type :keep
 			cardinal :se
 			location 25620, -20251.2
+			link '[&BOIDAAA=]'
 		end
 
 		objective do
@@ -161,6 +179,7 @@ class Map
 			type :tower
 			cardinal :se
 			location 10137.6, -21256.8
+			link '[&BOgDAAA=]'
 		end
 
 		objective do
@@ -169,6 +188,7 @@ class Map
 			type :camp
 			cardinal :se
 			location 13310.4, -28867.2
+			link '[&BOcDAAA=]'
 		end
 
 		objective do
@@ -177,6 +197,7 @@ class Map
 			type :tower
 			cardinal :sse
 			location 21544.8, -27280.8
+			link '[&BOUDAAA=]'
 		end
 
 		objective do
@@ -185,6 +206,7 @@ class Map
 			type :castle
 			cardinal :c
 			location 2690.4, -5174.4
+			link '[&BN4DAAA=]'
 		end
 	end
 end
